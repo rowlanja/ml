c7cfe12e913cbd309efea440e2da7ef4f115b958
bazir21 <bayir@live.ie>
2021-12-01 04:51:02 +0000

    wrote little piece about choosing models and done a tiny bitta kfold cross validation

--- Modelling.py
+++ Modelling.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import PolynomialFeatures
@@ -24,22 +24,22 @@ y = np.array(y)
 
 mean_error = []
 std_error = []
-c_range = [0.001, 0.1, 1, 10]
+c_range = [0.01, 0.05, 0.1, 0.5, 1]
 
 for c in c_range:
     temp = []
-    model = LogisticRegression(penalty="l2", C=c)
+    #model = LinearRegression()
+    model = Lasso(alpha= 1 / (2 * c))
+    #model = Ridge(alpha= 1 / (2 * c))
     kf = KFold(n_splits=5)
-    for train, test in kf.split(x_poly):
+    for train, test in kf.split(x):
         model.fit(x[train], y[train])
         y_pred = model.predict(x[test])
-        temp.append(mean_squared_error(y[test], y_pred, squared=False))
+        temp.append(mean_squared_error(y[test], y_pred))
     mean_error.append(np.array(temp).mean())
     std_error.append(np.array(temp).std())
 
 plt.errorbar(c_range, mean_error, yerr=std_error, ecolor="red")
-plt.xlabel("C")
-plt.ylabel("Root mean square error")
-#plt.xlim((0, 10.1))
-#plt.xticks(np.arange(0, 10.01, 1))
+plt.xlabel("Ridge - C")
+plt.ylabel("Mean square error")
 plt.show()
